2a4116f64701fc018ece329b0615883390edce0b
Steven R. Brandt <sbrandt@cct.lsu.edu>
2019-05-08 14:39:56 -0500

    Still working to make the system general

--- auto.sh
+++ auto.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
 
 # Configuration
-export INSTALL_DIR=$HOME/phylanx/devenv
-export EMAIL=sbrandt@cct.lsu.edu
-export DOCKER_HUB_ACCT=stevenrbrandt
+# Find execution dir
+export INSTALL_DIR=$(dirname $(realpath $0))
+if [ "$EMAIL" = "" ]
+then
+  export EMAIL=$(grep 'email.*=' ~/.gitconfig|cut -d= -f2|sed 's/\s//g')
+else
+  echo "PLEASE SET UP YOUR GIT EMAIL"
+  echo "git config --global user.email johndoe@example.com"
+  exit 2
+fi
+
+# Here we want the dockerhub account with a / or an empty string.
+if [ "$DOCKER_HUB_ACCT" = "" ]
+then
+  export DOCKER_HUB_ACCT=$(docker info|grep Username:|cut -d: -f2|sed 's/\s//g'|sed 's|/*$|/|')
+fi
+
+export CPUS=$(($(lscpu|grep '^CPU(s):'|cut -d: -f2|sed 's/\s//g')/2))
+
+echo "CONFIGURATION INFO:"
+echo "EMAIL=($EMAIL)"
+echo "INSTALL_DIR=($INSTALL_DIR)"
+echo "DOCKER_HUB_ACCT=($DOCKER_HUB_ACCT)"
+echo "CPUS=${CPUS}"
 
 source ~/.bashrc
 set -e
 set -x
 cd $INSTALL_DIR
 docker pull fedora
-docker build --no-cache -f phylanx.devenv -t stevenrbrandt/phylanx.devenv .
+docker build --no-cache --build-arg CPUS=$CPUS -f phylanx.devenv -t ${DOCKER_HUB_ACCT}phylanx.devenv .
+
+cat > test.docker <<EOF
+FROM ${DOCKER_HUB_ACCT}phylanx.devenv
+RUN build.sh install tests > test-out.txt 2>&1
+COPY versions.sh .
+RUN bash versions.sh >> test-out.txt
+EOF
 docker build -f test.docker -t phylanx-test .
 docker run --rm phylanx-test cat test-out.txt > test-out.txt
 python3 parse.py 
@@ -18,19 +46,18 @@ echo $EMAIL > email-body-1.html
 echo 'Phylanx Build Status' >> email-body-1.html
 if [ $? = 0 ]
 then
-  docker tag $DOCKER_HUB_ACCT/phylanx.devenv:latest $DOCKER_HUB_ACCT/phylanx.devenv:working
+  docker tag ${DOCKER_HUB_ACCT}phylanx.devenv:latest ${DOCKER_HUB_ACCT}phylanx.devenv:working
   bash push.sh
   set +e
   docker stop devenv
   sleep 5
-  docker run --name devenv --privileged -v devenv_homefs-phylanx:/home/jovyan -d --rm stevenrbrandt/phylanx.devenv:working 
+  docker run --name devenv --privileged -v devenv_homefs-phylanx:/home/jovyan -d --rm ${DOCKER_HUB_ACCT}phylanx.devenv:working 
   set -e
-  echo '<h1>Succeeded in building phylanx devenv.</h1>' >> email-body-1.html
+  echo 'Succeeded in building phylanx devenv.' >> email-body-1.txt
 else
-  echo '<h1>Failed to build phylanx devenv.</h1>' >> email-body-1.html
+  echo 'Failed to build phylanx devenv.' >> email-body-1.txt
 fi
-echo '<pre>' >> email-body-1.html
+echo '' >> email-body-1.txt
 touch test-out.txt
-tail -20 test-out.txt >> email-body-1.html
-echo '</pre>' >> email-body-1.html
-email.pl email-body-1.html
+tail -20 test-out.txt >> email-body-1.txt
+mail -s "Auto Update Phylanx" ${EMAIL} < email-body-1.txt
--- push.sh
+++ push.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-docker push $DOCKER_HUB_ACCT/phylanx.devenv:working
-ssh -p 8000 rostam singularity build images/phylanx-devenv.simg docker://$DOCKER_HUB_ACCT/phylanx.devenv:working
+
+if [ "$DOCKER_HUB_ACCT" != "" ]
+then
+  echo docker push ${DOCKER_HUB_ACCT}phylanx.devenv:working
+fi
+
+# Push to remote machine if there is one.
+if [ "${REMOTE}" != "" ]
+then
+  ssh ${REMOTE} singularity build ~/images/phylanx-devenv.simg docker://$DOCKER_HUB_ACCT/phylanx.devenv:working
+fi
